4cb1779ea035560d1ce1d7429bd32c09a05bd9d7
Bashir Musa <bashshow14@gmail.com>
2023-02-23 20:37:48 +0000

    updated

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - Prints it's string argument to console.
+ * Return: Returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
