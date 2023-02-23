ebe30b5909ba0bf4a54863fcec1083b4f40e09ae
Bashir Musa <bashshow14@gmail.com>
2023-02-23 20:19:55 +0000

    updated

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - Prints it's string arguments on to the console
+ * Return: Returns 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
