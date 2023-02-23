47b0de3a3dd9f4488e90d7743e6df3803298f339
Bashir Musa <bashshow14@gmail.com>
2023-02-23 22:34:29 +0000

    updated

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - entry point for our program
+ * Return: Returns 0 (success)
+ */
+int main(void)
+{
+	fprint(stderr, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
+	return (1);
+}
