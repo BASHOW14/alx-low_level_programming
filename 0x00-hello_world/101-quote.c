73e7638bed5055cfffa6109ad569f8a648b8ce83
Bashir Musa <bashshow14@gmail.com>
2023-02-24 13:00:17 +0000

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,6 @@
  */
 int main(void)
 {
-	char s[60];
-
-	s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fprintf(stderr, s);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 	return (1);
 }
