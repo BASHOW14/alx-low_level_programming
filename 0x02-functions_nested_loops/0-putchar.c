9e559bd113e340c077a9d1afba092406cbeb93e3
Bashir Musa <bashshow14@gmail.com>
2023-02-26 07:25:19 +0000

    updated

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+/**
+ * main - starting point of the program
+ * Return: Returns 0 (success);
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	i = 0;
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar(\n);
+	return (0);
+}
