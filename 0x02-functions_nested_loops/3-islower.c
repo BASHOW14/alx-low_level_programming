03cb6e78dfbde280b28940acc2345ca4eacc9633
Bashir Musa <bashshow14@gmail.com>
2023-02-26 09:46:42 +0000

    updated,

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,14 +3,10 @@
  *_islower - checks wether a character is lower
  * Return: Returns 1 (success) 0 (otherwise)
  *@c: the letter to check if its lower case
- *@res - stores return an integer value of _putchar function
  */
 int _islower(int c)
 {
-	int res;
-
-	res = _putchar(c);
-	if (res >= 97 && res <= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
