f8cd7ff478c738000c99b098d38d35c985c0610d
Bashir Musa <bashshow14@gmail.com>
2023-02-27 12:48:11 +0000

    updated

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - count the lenght of a string
+ * Return: Returns the lengh of a string in bytes
+ * @s: variable that points to address of the string in memory
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	return (i);
+i}
