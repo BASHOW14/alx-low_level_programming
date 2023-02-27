21ecb463e5a9ef0b5a8ec93560728004d26d55bc
Bashir Musa <bashshow14@gmail.com>
2023-02-27 16:20:33 +0000

    updated

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - prints string in reverse.
+ * @s:contains the address of string in memory.
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
+
