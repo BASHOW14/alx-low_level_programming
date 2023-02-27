6e8aac05bc5f4a4e6b62bfe5266a8053932c7d02
Bashir Musa <bashshow14@gmail.com>
2023-02-27 10:07:50 +0000

    updated

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value of it's argument to 98
+ * @n: parameter that stores the address of the variable
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
