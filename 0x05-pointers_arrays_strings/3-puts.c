0671349be8836f312c981f765e4cf892b44d457e
Bashir Musa <bashshow14@gmail.com>
2023-02-27 13:47:14 +0000

    updated

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints its arguments to standard output.
+ * @str:the argument to print to stdout.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != 0)
+	{
+		_putchar(str[i]);
+		i++
+	}
+	Putchar('\n');
+}
