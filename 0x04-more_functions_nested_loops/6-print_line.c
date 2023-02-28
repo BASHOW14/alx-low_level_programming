0f51ae03dd14f46b47a98d990ff005ddffccff95
Bashir Musa <bashshow14@gmail.com>
2023-02-28 22:47:17 +0000

    updated

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - print line on the screen.
+ * @n:the length of a line
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar(10);
+	}
+	else
+	{
+		int l;
+
+		l = 1;
+
+		while (l <= n)
+		{
+			_putchar(95);
+		}
+		_putchar(10);
+	}
+}
