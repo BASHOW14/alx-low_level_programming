3dba7962c5817a26f3ad3d8740cdf8f6c9e91ff3
Bashir Musa <bashshow14@gmail.com>
2023-02-28 22:07:12 +0000

    updated

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers between 0-9 excluding 2 & 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 || i != 52)
+			_putchar(i);
+		else
+			continue;
+	}
+	_putchar(10);
+}
