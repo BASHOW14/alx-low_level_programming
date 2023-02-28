18f13696adfae8322f59b08f29d0c1826a2e1512
Bashir Musa <bashshow14@gmail.com>
2023-02-28 19:45:32 +0000

    updated

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character
+ * @str:stores the address of the character string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != 0)
+	{
+		_putchar(s[i]);
+		i = i + 2;
+	}
+}
