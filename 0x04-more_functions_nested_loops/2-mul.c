6c538d816401bc27f967f4183d7eed4548801101
Bashir Musa <bashshow14@gmail.com>
2023-02-28 21:39:57 +0000

    updated

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *mul - multiplies two numbers
+ *Return: Returns the product of the two numbers
+ *@a:first argument
+ *@b:second argument
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
