78d09a41cdb5bb37461ac1810fe7dfa05ca3df64
Bashir Musa <bashshow14@gmail.com>
2023-03-01 22:26:34 +0000

    updated

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - print a square on the terminal
+ * @size:the size of a square
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	if (size <= 0)
+	{
+		_putchar(10);
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= n; j++)
+			{
+				putchar(35);
+			}
+		}
+		_putchar(10);
+	}
+}
