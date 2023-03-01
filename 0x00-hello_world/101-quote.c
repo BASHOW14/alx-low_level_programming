93c65f995edeca176bd5d7441e6b0d6f4b3809fb
Bashir Musa <bashshow14@gmail.com>
2023-03-01 16:38:53 +0000

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
+#include<unistd.h>
 /**
  * main - entry point for our program
  * Return: Returns 0 (success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	char *s;
+
+	s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, &s, 58);
 	return (1);
 }
