cb8d6b1720afa5ed5f3ce9ca996ecca3bcf708e5
Bashir Musa <bashshow14@gmail.com>
2023-03-02 22:20:28 +0000

    updated

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * reverse_array - reverse an element of the array
+ * @n:number of element in the array
+ * @a:a pointer to the array
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temp;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		;
+	}
+
+	while (j < i)
+	{
+		a[j] = temp;
+		a[j] = a[i - 1]
+		a[i] = temp;
+		j++;
+		i--;
+	}
+}
+
