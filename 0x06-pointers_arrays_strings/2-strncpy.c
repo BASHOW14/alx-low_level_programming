032b0313e652667b57ca057b4263e22a7d73fe6c
Bashir Musa <bashshow14@gmail.com>
2023-03-02 21:00:14 +0000

    updated

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,16 +9,11 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int j;
 
-	j = 0;
-
-	for (j = 0; j < n && s[j] != '\0'; j++)
+	for (i = 0; i < n && s[i] != '\0'; i++)
 	{
-		dest[i] = src[j];
+		dest[i] = src[i];
 	}
-	for (j = 0; j < n; j++)
-		dest[j] = '\0';
 
 	return (dest);
 }
