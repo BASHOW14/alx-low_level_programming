a952c417afc3c9cdb0d84d6b243536ec2ec5c7c0
Bashir Musa <bashshow14@gmail.com>
2023-03-02 11:22:10 +0000

    updated

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncpy - copies src string to dest string
+ * Return: Returns pointer to dest string
+ * @dest:destination string
+ * @src:source string
+ * @n:number of bytes to be written to the buffer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; j < n && s[j] != '\0')
+	{
+		dest[i + j] = src[j];
+	}
+	dest[j + i] = '\0';
+	return (dest);
+}
+
+
