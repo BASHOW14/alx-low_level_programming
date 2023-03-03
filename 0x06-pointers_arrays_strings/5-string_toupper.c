cacbb288c96376dab94dc7201dd5258a99cc4397
Bashir Musa <bashshow14@gmail.com>
2023-03-03 06:41:49 +0000

    updated

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * string_toupper - converts string to uppercase
+ * Return: Returns a pointer to the updated string
+ * @s:strint to be coverted.
+ */
+char *string_toupper(char *s)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			j = s[i] - 32;
+		}
+	}
+	return (s);
+}
+
