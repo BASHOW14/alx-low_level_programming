eb74c92cc0596a9f0f88759a4e265a860b0aa6d4
Bashir Musa <bashshow14@gmail.com>
2023-03-04 16:12:32 +0000

    updated

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - print numbers from n to 98
+ * @n: operand
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != n)
+				printf("%d, ", i);
+			else
+				printf(" %d", i);
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != n)
+				printf("%d, ", i);
+			else
+				printf(" %d", i);
+		}
+	}
+}
