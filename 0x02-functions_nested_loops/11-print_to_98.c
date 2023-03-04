47f63b1834635534e28d576e549a84450e5861ef
Bashir Musa <bashshow14@gmail.com>
2023-03-04 16:41:26 +0000

    updated

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -26,13 +26,13 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			if (i < 98)
+			if (i > 98)
 			{
 				printf("%d, ", i);
 			}
 			else
 			{
-				printf("%d ", i);
+				printf("%d", i);
 			}
 
 		}
